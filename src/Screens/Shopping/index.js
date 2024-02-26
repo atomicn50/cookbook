31b7c1d12a7617e53ec381e1c3936ba33ebaeac9
tinh <tinhkle@gmail.com>
2024-02-26 11:54:41 -0800

    move styles to separate file

--- src/Screens/Shopping/index.js
+++ src/Screens/Shopping/index.js
@@ -11,62 +11,7 @@ import {
 import { Fontisto, Ionicons, MaterialCommunityIcons } from '@expo/vector-icons';
 import { isEmpty } from 'lodash-es';
 
-const styles = StyleSheet.create({
-  inputContainer: {
-    flexDirection: 'row',
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  input: {
-    height: 40,
-    width: 315,
-    marginHorizontal: 12,
-    marginTop: 20,
-    marginBottom: 10,
-    borderWidth: 1,
-    padding: 10,
-    fontSize: 16,
-    fontWeight: '500',
-    backgroundColor: 'whitesmoke'
-  },
-  ingredientContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    marginHorizontal: 12,
-    margin: 4,
-  },
-  ingredient: {
-    fontSize: 18,
-  },
-  ingredientQuantity: {
-    padding: 2,
-    fontSize: 18,
-  },
-  boughtIngredientsContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    marginLeft: 12,
-    marginBottom: 8,
-  },
-  boughtIngredientsHeader: {
-    margin: 12,
-    fontSize: 20,
-    color: 'orangered',
-    fontWeight: 'bold'
-  },
-  boughtIngredientQuantity: {
-    padding: 2,
-    fontSize: 18,
-    color: 'dimgray',
-    textDecorationLine: 'line-through',
-  },
-  boughtIngredient: {
-    fontSize: 18,
-    color: 'dimgray',
-    textDecorationLine: 'line-through',
-  },
-});
-
+import styles from './styles';
 
 export default function Shopping() {
   const [input, setInput] = useState('');
