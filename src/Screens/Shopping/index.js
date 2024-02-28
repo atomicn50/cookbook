17f43bc50503844b734f8be2afe79ff1c4e61a1e
tinh <tinhkle@gmail.com>
2024-02-28 12:41:23 -0800

    refactor components for readability

--- src/Screens/Shopping/index.js
+++ src/Screens/Shopping/index.js
@@ -14,55 +14,47 @@ import styles from './styles';
 export default function Shopping() {
   const [input, setInput] = useState('');
   const [ingredients, setIngredients] = useState({});
-  const boughtIngredients = useMemo(() => (
+  const boughtIngredients = (
     Object.entries(ingredients)
-      .filter(([_, ingredientMetadata]) => ingredientMetadata.hasIngredientBeenBought)
-  ), [ingredients]);
+      .filter(([_, { hasIngredientBeenBought }]) => hasIngredientBeenBought)
+  )
+
+  const handleOnPress = () => {
+    if (input && ingredients[input]) {
+      setIngredients(prevIngredients => ({
+        ...prevIngredients,
+        [input]: {
+          hasIngredientBeenBought: false,
+          quantity: prevIngredients[input].quantity + 1,
+        },
+      }));
+    } else {
+      input && setIngredients(prevIngredients => ({
+        ...prevIngredients,
+        [input]: {
+          hasIngredientBeenBought: false,
+          quantity: 1,
+        },
+      }));
+    }
+  };
 
   return (
     <View style={{backgroundColor: 'white', height: '100%'}}>
       <View>
-        <View
-          style={styles.inputContainer}
-        >
-        <TextInput
-          style={styles.input}
-          placeholder='Add item'
-          onChangeText={(i) =>setInput(i)}
-        >
-        </TextInput>
-        <TouchableOpacity
-          onPress={() => {
-            if (input && ingredients[input]) {
-              setIngredients(prevIngredients => ({
-                ...prevIngredients,
-                [input]: {
-                  hasIngredientBeenBought: false,
-                  quantity: prevIngredients[input].quantity + 1,
-                },
-              }));
-            } else {
-              input && setIngredients(prevIngredients => ({
-                ...prevIngredients,
-                [input]: {
-                  hasIngredientBeenBought: false,
-                  quantity: 1,
-                },
-              }));
-            }
-          }}
-        >
-          <Fontisto name="shopping-basket-add" size={24} color="black" />
-        </TouchableOpacity>
+        <View style={styles.inputContainer}>
+          <TextInput
+            style={styles.input}
+            placeholder='Add item'
+            onChangeText={(i) =>setInput(i)}
+          >
+          </TextInput>
+          <TouchableOpacity onPress={handleOnPress}>
+            <Fontisto name="shopping-basket-add" size={24} color="black" />
+          </TouchableOpacity>
         </View>
-        <IngredientList
-          ingredients={ingredients}
-          setIngredients={setIngredients}
-        />
-         <BoughtIngredients
-          boughtIngredients={boughtIngredients}
-          setIngredients={setIngredients}
-        />
+        <IngredientList ingredients={ingredients} setIngredients={setIngredients}/>
+        <BoughtIngredients boughtIngredients={boughtIngredients} setIngredients={setIngredients}/>
       </View>
     </View>
   );
