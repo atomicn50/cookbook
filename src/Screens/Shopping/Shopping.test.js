0df3917b5daff9a6c5bf6aaa69191251fb26cf56
tinh <tinhkle@gmail.com>
2024-02-29 15:48:48 -0800

    test that ingredients and quantity show after pressing button

--- src/Screens/Shopping/Shopping.test.js
+++ src/Screens/Shopping/Shopping.test.js
@@ -8,12 +8,46 @@ describe('Shopping', () => {
     render(<Shopping />);
   });
 
-  test('should show input bar', () => {
-    expect(screen.getByPlaceholderText('Add item')).toBeTruthy();
+  describe('on first render', () => {
+    test('should show input bar', () => {
+      expect(screen.getByPlaceholderText('Add item')).toBeTruthy();
+    });
+  
+    test('should show the add ingredient button', () => {
+      expect(screen.getByRole('button')).toBeTruthy();
+    });
   });
 
-  test('should show the add ingredient button', () => {
-    expect(screen.getByRole('button')).toBeTruthy();
+  describe('after pressing add ingredient button', () => {
+    test('should see ingredient appear in list', () => {
+      // arrange
+      const textInput = screen.getByPlaceholderText('Add item');
+      const button = screen.getByRole('button');
+
+      // act
+      fireEvent.changeText(textInput, 'Beans');
+      fireEvent.press(button);
+
+      // assert
+      expect(screen.getByText('Beans')).toBeTruthy();
+    });
+  });
+
+  describe('after pressing add ingredient button twice with same ingredient', () => {
+    test('should see ingredient and quantity appear in list', () => {
+      // arrange
+      const textInput = screen.getByPlaceholderText('Add item');
+      const button = screen.getByRole('button');
+
+      // act
+      fireEvent.changeText(textInput, 'Fish');
+      fireEvent.press(button);
+      fireEvent.press(button);
+
+      // assert
+      expect(screen.getByText('(2)')).toBeTruthy();
+      expect(screen.getByText('Fish')).toBeTruthy();
+    });
   });
 });
 
