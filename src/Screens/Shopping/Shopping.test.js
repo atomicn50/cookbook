3e996641d209c4cff9e42ef0c6199ea476e41812
tinh <tinhkle@gmail.com>
2024-02-29 15:42:00 -0800

    add test

--- src/Screens/Shopping/Shopping.test.js
+++ src/Screens/Shopping/Shopping.test.js
@@ -1,15 +1,19 @@
-import { describe, expect, test } from '@jest/globals';
+import { describe, expect, test, beforeEach } from '@jest/globals';
 import { render, screen, fireEvent } from '@testing-library/react-native';
 
 import Shopping from './Shopping';
 
 describe('Shopping', () => {
-  test('should show input bar', () => {
-    // act
+  beforeEach(() => {
     render(<Shopping />);
+  });
 
-    // assert
+  test('should show input bar', () => {
     expect(screen.getByPlaceholderText('Add item')).toBeTruthy();
+  });
+
+  test('should show the add ingredient button', () => {
+    expect(screen.getByRole('button')).toBeTruthy();
   });
 });
 
