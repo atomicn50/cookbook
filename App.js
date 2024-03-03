8b855315371cec4fe6dda7ae463e0d7b9d233f51
tinh <tinhkle@gmail.com>
2024-03-02 16:00:10 -0800

    lint

--- App.js
+++ App.js
@@ -1,4 +1,3 @@
-import { StyleSheet } from 'react-native';
 import { NavigationContainer  } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
@@ -54,12 +53,3 @@ export default function App() {
     </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
